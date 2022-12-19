505d5b6e985d83037030e5fc6fd3d58bd23b3856
Yannick Vanhaeren <yannick.vanhaeren@gmail.com>
2022-12-19 15:02:22 +0100

    Change last php version to 8.2

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -ex
 DOCKER_PHP_REPO="yannickvh/php"
 DOCKER_PHP_PROD_REPO="yannickvh/php-prod"
 DOCKER_PHP_DEV_REPO="yannickvh/php-dev"
-LAST_PHP_VERSION="8.1"
+LAST_PHP_VERSION="8.2"
 php_version=$1
 git_branch=$1
 build_arg=""
