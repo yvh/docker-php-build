e046974635f320eebee1103740bc619c364b10e1
Yannick Vanhaeren <yannick.vanhaeren@gmail.com>
2023-04-17 10:47:22 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ set -ex
 DOCKER_PHP_REPO="yannickvh/php"
 DOCKER_PHP_PROD_REPO="yannickvh/php-prod"
 DOCKER_PHP_DEV_REPO="yannickvh/php-dev"
-LAST_PHP_VERSION="8.2"
 php_version=$1
 git_branch=$1
 build_arg=""
@@ -13,9 +12,6 @@ if [[ ! -z "$2" ]]; then
     build_arg="--build-arg http_proxy=$2 --build-arg https_proxy=$2"
 fi
 
-if [[ $php_version = $LAST_PHP_VERSION ]]; then
-  git_branch="main"
-fi
 
 for i in docker-php docker-php-prod docker-php-dev; do
   cd ../$i
