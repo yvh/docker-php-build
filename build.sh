efd1b04a8d615ab4744686cbde16d328fa484779
Yannick Vanhaeren <yannick.vanhaeren@gmail.com>
2023-08-10 20:09:56 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -1,38 +1,36 @@
 #!/usr/bin/env bash
-set -ex
+set -e
 
 DOCKER_PHP_REPO="yannickvh/php"
 DOCKER_PHP_PROD_REPO="yannickvh/php-prod"
 DOCKER_PHP_DEV_REPO="yannickvh/php-dev"
-php_version=$1
-git_branch=$1
+GREEN='\033[0;32m' # Green color
+NC='\033[0m' # No color
+php_version=${1}
 build_arg=""
 
-if [[ ! -z "$2" ]]; then
-    build_arg="--build-arg http_proxy=$2 --build-arg https_proxy=$2"
+if [[ ! -z "${2}" ]]; then
+    build_arg="--build-arg http_proxy=${2} --build-arg https_proxy=${2}"
 fi
 
+project_path=$(pwd)/..
 
 for i in docker-php docker-php-prod docker-php-dev; do
-  cd ../$i
-  echo "Current dir: " $(pwd)
-  git fetch
-  git checkout $git_branch
+  pushd ${project_path}/${i}/${php_version}
+  echo -e "${GREEN}Current dir: $(pwd)${NC}"
 
-  if [[ $i =~ 'prod' ]]; then
-    docker_repo=$DOCKER_PHP_PROD_REPO
+  if [[ ${i} =~ 'prod' ]]; then
+    docker_repo=${DOCKER_PHP_PROD_REPO}
   elif [[ $i =~ 'dev' ]]; then
-    docker_repo=$DOCKER_PHP_DEV_REPO
+    docker_repo=${DOCKER_PHP_DEV_REPO}
   else
-    docker_repo=$DOCKER_PHP_REPO
+    docker_repo=${DOCKER_PHP_REPO}
   fi
 
   for j in apache cli fpm; do
-    tag=$php_version-$j
-    echo "Build $docker_repo:$tag"
-    docker build --no-cache $build_arg -t $docker_repo:$tag $j
-    docker push $docker_repo:$tag
+    tag=${php_version}-${j}
+    echo -e "${GREEN}Building ${docker_repo}:${tag}${NC}"
+    docker build --no-cache ${build_arg} -t ${docker_repo}:${tag} ${j}
+    docker push ${docker_repo}:${tag}
   done
-
-  git checkout -
 done
